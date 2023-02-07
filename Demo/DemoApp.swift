3325d50a4d950746a8080e1846943d42da1426d4
rokin <rokin@meishakeji.com>
2023-02-07 17:58:20 +0800

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by MSEDU_iOS on 2023/2/7.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
